f48472f0afea2199ea090020569dd00bce79ff87
vats98754 <a7vats@uwaterloo.ca>
2025-08-21 05:08:33 +0530

    scaffolding canny edge detector (TBD from scratch) and CNNs (also TBD from scratch)

---
+++ canny_edge_detection/FromScratchCanny.py
@@ -0,0 +1,7 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+class FromScratchCanny:
+    def __init__(self, img):
+        self.img = img
---
+++ deep_cnn/FromScratchCNN.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+
+class FromScratchCNN:
+    def __init__(self):
+        self.layers = 5
+        self.img = 0
+        self.layer_sizes = []
